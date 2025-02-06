830ba3d3b8f119fccd04c394d30879eb24c26dc6
hayoonPark <the.day0619@gmail.com>
2025-02-06 16:26:31 +0900

    update: 스플래시 화면 추가

---
+++ day2/app/src/main/java/com/example/twice/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.twice
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()  // 스플래시 액티비티는 끝
+        }, 3000)  // 3초 뒤에 메인 액티비티로 넘긴다.
+    }
+}
