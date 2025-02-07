d336ecca6e1d56d5978923df824690b4e055f81c
hayoonPark <the.day0619@gmail.com>
2025-02-07 16:52:45 +0900

    update: 랜덤값으로 주사위 놀이 추가

--- day3/dice/app/src/main/java/com/example/dice/MainActivity.kt
+++ day3/dice/app/src/main/java/com/example/dice/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.dice
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.dice.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,46 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val dice1 = binding.dice1
+        val dice2 = binding.dice2
+
+        binding.diceStartBtn.setOnClickListener {
+
+            Toast.makeText(this, "주사위 굴러가유~", Toast.LENGTH_LONG).show()
+
+            val i = Random.nextInt(1, 7)
+            val j = Random.nextInt(1, 7)
+
+            if (i == 1) {
+                dice1.setImageResource(R.drawable.dice_1)
+            } else if (i == 2) {
+                dice1.setImageResource(R.drawable.dice_2)
+            } else if (i == 3) {
+                dice1.setImageResource(R.drawable.dice_3)
+            } else if (i == 4) {
+                dice1.setImageResource(R.drawable.dice_4)
+            } else if (i == 5) {
+                dice1.setImageResource(R.drawable.dice_5)
+            } else if (i == 6) {
+                dice1.setImageResource(R.drawable.dice_6)
+            }
+
+            if (j == 1) {
+                dice2.setImageResource(R.drawable.dice_1)
+            } else if (j == 2) {
+                dice2.setImageResource(R.drawable.dice_2)
+            } else if (j == 3) {
+                dice2.setImageResource(R.drawable.dice_3)
+            } else if (j == 4) {
+                dice2.setImageResource(R.drawable.dice_4)
+            } else if (j == 5) {
+                dice2.setImageResource(R.drawable.dice_5)
+            } else if (j == 6) {
+                dice2.setImageResource(R.drawable.dice_6)
+            }
         }
     }
 }
