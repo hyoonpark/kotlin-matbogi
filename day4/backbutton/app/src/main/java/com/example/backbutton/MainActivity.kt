afe120838828ac3961c4294f066b0ff68aa2b97d
hayoonPark <the.day0619@gmail.com>
2025-02-10 14:40:31 +0900

    update: 뒤로가기 두번 누르면 종료 기능

--- day4/backbutton/app/src/main/java/com/example/backbutton/MainActivity.kt
+++ day4/backbutton/app/src/main/java/com/example/backbutton/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.backbutton
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,22 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
+    }
+
+    private var backPressedTime = 0L
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - backPressedTime <= 2000) {
+                finish()
+            } else {
+                backPressedTime = System.currentTimeMillis()
+                Toast.makeText(this@MainActivity, "한 번 더 누르면 종료합니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
