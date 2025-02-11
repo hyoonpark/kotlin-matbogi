fbf109f43f8c0a2db7aeac94b8a372cb9f348441
hayoonPark <the.day0619@gmail.com>
2025-02-11 14:05:44 +0900

    update: 간단한 listview

--- day5/listviewreview/app/src/main/java/com/example/listviewreview/MainActivity.kt
+++ day5/listviewreview/app/src/main/java/com/example/listviewreview/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.listviewreview
 
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,23 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val listItem = mutableListOf<String>()
+
+        listItem.add("A")
+        listItem.add("B")
+        listItem.add("C")
+
+        val listView = findViewById<ListView>(R.id.mainListView)
+
+        val adapter = ListViewAdapter(listItem)
+        listView.adapter = adapter
+        
+        listView.setOnItemClickListener { parent, view, position, id ->
+
+            Toast.makeText(this, listItem[position], Toast.LENGTH_LONG).show()
+
         }
     }
 }
