624f0347f75dc8e3383a0b81b45f5f89bf782be4
hayoonPark <the.day0619@gmail.com>
2025-02-11 14:41:39 +0900

    update: 리사이클러뷰 맛보기, 클릭이벤트 구현

--- day5/recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ day5/recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.recyclerview
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val items = mutableListOf<String>()
+
+        items.add("a")
+        items.add("b")
+        items.add("c")
+
+        val rv = findViewById<RecyclerView>(R.id.rv)
+
+        val adapter = RVAdapter(items)
+        rv.adapter = adapter
+        rv.layoutManager = LinearLayoutManager(this)
+
+        adapter.itemClick = object : RVAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                Toast.makeText(baseContext, items[position], Toast.LENGTH_LONG).show()
+            }
+
         }
     }
 }
