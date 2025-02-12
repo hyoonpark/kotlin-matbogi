84ada6bf5e578c8e285668a9da59fe3f7a790188
hayoonPark <the.day0619@gmail.com>
2025-02-12 15:18:22 +0900

    update: 이메일 회원가입, 로그인, 로그아웃

--- day6/fbemaillogin/app/src/main/java/com/example/fbemaillogin/MainActivity.kt
+++ day6/fbemaillogin/app/src/main/java/com/example/fbemaillogin/MainActivity.kt
@@ -1,12 +1,72 @@
 package com.example.fbemaillogin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        auth = Firebase.auth
+
+        // 이메일 회원가입
+        val join = findViewById<Button>(R.id.joinBtn)
+        join.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.emailArea)
+            val pw = findViewById<EditText>(R.id.pwArea)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), pw.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+
+                        Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+
+        // 이메일 로그인
+        val login = findViewById<Button>(R.id.loginBtn)
+        login.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.emailArea)
+            val pw = findViewById<EditText>(R.id.pwArea)
+
+            auth.signInWithEmailAndPassword(email.text.toString(), pw.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT,).show()
+                    }
+                }
+
+        }
+
+        // 로그아웃
+        val logout = findViewById<Button>(R.id.logoutBtn)
+        logout.setOnClickListener {
+            Firebase.auth.signOut()
+            Toast.makeText(this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
