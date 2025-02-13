4fabed66c5e5ee0c5daa0ce24818fe570121e2e5
hayoonPark <the.day0619@gmail.com>
2025-02-13 15:25:58 +0900

    update: 이메일 회원가입 기능 추가

--- day7/hachelin/app/build.gradle.kts
+++ day7/hachelin/app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     // firebase
     implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
 }
--- day7/hachelin/app/src/main/java/com/example/hachelin/SplashActivity.kt
+++ day7/hachelin/app/src/main/java/com/example/hachelin/SplashActivity.kt
@@ -3,20 +3,38 @@ package com.example.hachelin
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 @Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 2500)
+        auth = Firebase.auth
+
+        if (auth.currentUser?.uid == null) {
+            // 회원가입 필요
+            Handler().postDelayed({
+                startActivity(Intent(this, JoinActivity::class.java))
+                finish()
+            }, 2500)
+
+        } else {
+            // 메인화면으로 이동
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 2500)
+
+        }
+
+
     }
 }
